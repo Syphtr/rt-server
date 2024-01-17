7485739cdab428c21bf3d55b850078e3749d15cd
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-01-17 17:05:42 +0000

    forward ref prisma service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { type User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    @Inject(forwardRef(() => PrismaService)) private prisma: PrismaService,
+  ) {}
 
   async createUser(data: any): Promise<User> {
     return this.prisma.user.create({
