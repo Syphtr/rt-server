e4295269c3b181ad483ad04c0998f5062752c6a9
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-01-17 17:03:10 +0000

    update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { type User, type Prisma } from '@prisma/client';
+import { type User } from '@prisma/client';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {
+  async createUser(data: any): Promise<User> {
     return this.prisma.user.create({
       data,
     });
