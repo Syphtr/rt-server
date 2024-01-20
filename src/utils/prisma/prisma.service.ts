e82a9ba738f6f17b095920c9f0fc4637fbb671c1
TbcProjects <154512269+Recruitpea@users.noreply.github.com>
2024-01-20 13:48:07 +0000

    feat: implement prisma disconnect

--- src/utils/prisma/prisma.service.ts
+++ src/utils/prisma/prisma.service.ts
@@ -1,10 +1,22 @@
-import { Injectable, Global, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Global,
+  OnModuleInit,
+  OnModuleDestroy,
+} from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Global()
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit() {
     await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
